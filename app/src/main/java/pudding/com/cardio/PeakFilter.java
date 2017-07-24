87741503edc783f7b06111d274d7bde9364f25d9
Zhu Zhan Yan <program.nom@gmail.com>
2017-07-24 20:15:48 +0800

    Add PeakFilter.

---
+++ app/src/main/java/pudding/com/cardio/PeakFilter.java
@@ -0,0 +1,95 @@
+package pudding.com.cardio;
+
+import java.util.ArrayList;
+
+public class PeakFilter {
+
+    //Filter Parameters
+    private int logSize;
+    private double peakThreshold;
+    private ArrayList<Double> log;
+
+    //Cache
+    private boolean cacheMeanValid;
+    private double cacheMean;
+    private boolean cacheStandardDeviationValid;
+    private double cacheStandardDeviation;
+
+    public PeakFilter()
+    {
+        this.logSize = 3;
+        this.peakThreshold = 1.0;
+        this.log = new ArrayList<>();
+
+        this.cacheMeanValid = false;
+        this.cacheStandardDeviationValid = false;
+        this.cacheMean = 0.0;
+        this.cacheStandardDeviation = 0.0;
+    }
+
+    public void seed(double value)
+    {
+        //Invalidate Cache
+        this.cacheMeanValid = false;
+        this.cacheStandardDeviationValid = false;
+
+        if(this.log.size() == logSize) this.log.remove(this.log.size() - 1);
+        this.log.add(value);
+    }
+
+    public boolean determinePeak(double value)
+    {
+        if(this.log.size() > this.logSize)
+        {
+            double deviation =  value - this.computeMean();
+            double zScore = deviation / this.computeStandardDeviation();
+
+            if(zScore > this.peakThreshold) return true; //Found Peak
+        }
+
+        this.seed(value);
+        return false;
+    }
+
+
+    //Utility Methods
+    private double computeMean()
+    {
+        if(cacheMeanValid == true) return this.cacheMean;
+
+        double sum = 0.0;
+        for(double value : this.log) sum += value;
+
+        this.cacheMean = sum / ((double)this.log.size());
+        this.cacheMeanValid = true;
+
+        return this.cacheMean;
+    }
+
+    private double computeStandardDeviation()
+    {
+        if(cacheStandardDeviationValid == true) return this.cacheStandardDeviation;
+
+        double sum = 0.0;
+        for(double value : this.log) sum += Math.pow((value - this.computeMean()), 2);
+
+        this.cacheStandardDeviation = Math.sqrt((sum / ((double)this.log.size())));
+        this.cacheStandardDeviationValid = true;
+
+        return this.cacheStandardDeviation;
+    }
+
+
+    //Setters - Filter Parameters
+    public void setLogSize(int logSize) {
+        //Invalidate Cache
+        this.cacheMeanValid = false;
+        this.cacheStandardDeviationValid = false;
+
+        this.logSize = logSize;
+    }
+
+    public void setPeakThreshold(double peakThreshold) {
+        this.peakThreshold = peakThreshold;
+    }
+}
